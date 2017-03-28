787098542a2b16c13c6f5007e47e9b94a0a24089
BuildTools <fccccc.cf>
2017-03-28 20:52:38 +0500

    Songster update

--- SongsterrPremium/src/patchState.js
+++ SongsterrPremium/src/patchState.js
@@ -1,11 +1,16 @@
 //New (React.JS based) songsterr
-function applyPatch(){
-  if(window.__INITIAL_STATE__&&window.__INITIAL_STATE__.user)
-    __INITIAL_STATE__.user.hasPlus=true;
+console.log('Hello, songsterr admins! I love songsterr, but i am student, and it costs sooo much...');
+function f6cf___INITIAL_STATE___applyPatch(){
+    //New patch (yay)
+    Object.keys(window).filter(k=>k.indexOf('__')===0).forEach(k=>window[k]&&window[k].user&&window[k].user.hasPlus===false&&(window[k].user.hasPlus=true));
+    //I'll just leave it here
+    if(window.__INITIAL_STATE__&&window.__INITIAL_STATE__.user)
+        __INITIAL_STATE__.user.hasPlus=true;
 }
 let patcher = document.createElement('script');
-patcher.innerHTML = applyPatch+';applyPatch()';
-patcher.onload = function() {
-    this.remove();
-};
+patcher.innerHTML = applyPatch+';f6cf___INITIAL_STATE___applyPatch()';
+//Because it not always works as excepted
+// patcher.onload = function() {
+//     this.remove();
+// };
 (document.head || document.documentElement).appendChild(patcher);
