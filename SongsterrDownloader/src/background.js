b4ac89a2f1f9d6b61d2ca8a350fd70d52fc5bcb9
BuildTools <fccccc.cf>
2017-03-29 12:17:58 +0500

    Update downloader

--- SongsterrDownloader/src/background.js
+++ SongsterrDownloader/src/background.js
@@ -1,24 +1,31 @@
-function Download(){
-	var regexp = new RegExp("http\:\/\/www\.songsterr\.com\/a\/wsa\/");
-	var res = regexp.test(window.location);
-	if (res){
-		id=document.documentElement.innerHTML.split("songId=")[1].split("&base=")[0];
-		response="";
-		var xhReq = new XMLHttpRequest();
- 		xhReq.open("GET", "http://www.songsterr.com/a/ra/player/song/"+id+".xml", false);
- 		xhReq.send(null);
- 		response = xhReq.responseText;
-    	url=response.split("<attachmentUrl>")[1].split("</attachmentUrl>")[0];
-    	window.location=url;
-    	alert("Started downloading.")
-	}
-	else
-	{
-		alert("Not on songsterr, cancelling.")
+function f6cf___INITIAL_STATE___download(){
+	let id = window.location.pathname.split('-').slice(-1)[0].substr(1).split('t')[0]*1;
+	let apiUrl = `https://www.songsterr.com/api/v2/song/${id}`;
+	// Using promise since old versions of chrome doesn't supports async/await
+	fetch(apiUrl).then(res=>res.json()).then(data=>{
+		let format=data.source.split('.').slice(-1)[0];
+		let artist=data.artist || 'Unknown artist';
+		let title=data.title || 'Unknown title';
+		let name=`${artist} - ${title}.${format}`;
+		console.log(name);
+		downloadURI(data.source, name); 
+		// Well, chrome uses content-disposition instead of normal name...
+		// Because: http://stackoverflow.com/questions/23872902/chrome-download-attribute-not-working
+		// TODO: Proxy download url to songsterr origin
+	});
+
+	function downloadURI(url, name) {
+		let link = document.createElement('a');
+		link.download = name;
+		link.href = url;
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+		//delete link; because this line is useless, since garbage collector autimaticelly remove them
 	}
 }
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(tab=>{
   chrome.tabs.executeScript({
-    code: Download+'Download();'
+    code: f6cf___INITIAL_STATE___download+';f6cf___INITIAL_STATE___download();'
   });
 });
