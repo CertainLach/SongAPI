91548b253e7582bc0c50dbfcab68a8e4c529cba3
Yaroslaw Bolyukin <iam@f6cf.pw>
2017-10-24 13:09:46 +0500

    Patch interval, tempermonkey

--- SongsterrPremium/src/patchState.js
+++ SongsterrPremium/src/patchState.js
@@ -1,15 +1,23 @@
-//New (React.JS based) songsterr
+// ==UserScript==
+// @name Songsterr Premium
+// @version 0.1
+// @description Patch redux state to get premium features for free
+// @include *songsterr.com*
+// @include songsterr.com*
+// @include *songsterr.com
+// @include songsterr.com
+// @include www.songsterr.com*
+// @include http://songsterr.com/*
+// @include http://*.songsterr.com/*
+// ==/UserScript==
+
+// New (React.JS based) songsterr
 function f6cf___INITIAL_STATE___applyPatch(){
-    //New patch (yay)
-    Object.keys(window).filter(k=>/^_[a-z0-9]+_$/i.test(k)).forEach(k=>window[k]&&window[k].user&&window[k].user.hasPlus===false&&(window[k].user.hasPlus=true));
-    //I'll just leave it here
-    if(window.__INITIAL_STATE__&&window.__INITIAL_STATE__.user)
-        __INITIAL_STATE__.user.hasPlus=true;
+    setInterval(()=>{
+        // New patch (yay)
+        Object.keys(window).filter(k=>/^_[a-z0-9]+_$/i.test(k)).forEach(k=>window[k]&&window[k].user&&window[k].user.hasPlus===false&&(window[k].user.hasPlus=true));
+    },1000);
 }
 let patcher = document.createElement('script');
 patcher.innerHTML = f6cf___INITIAL_STATE___applyPatch+';f6cf___INITIAL_STATE___applyPatch()';
-//Because it not always works as excepted
-// patcher.onload = function() {
-//     this.remove();
-// };
 (document.head || document.documentElement).appendChild(patcher);
